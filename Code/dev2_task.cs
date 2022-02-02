264dd4d1508697c3d32d732d992a6b057fc820d1
Ganioree <93940235+Ganioree@users.noreply.github.com>
2022-02-02 10:15:04 +0300

    dev_task_array
    1

---
+++ Code/dev2_task.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int[] MyArray =
+        {
+             1 2,
+           3 4 5,
+          6 7 8 9
+          };
+        var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+
+
+
+
